9799db0272bd729f7ac0d5b92970b36cd9c54a50
yasuhiro <yasucom4646@gmail.com>
2019-08-20 19:46:11 -0700

    Create footer.component.jsx

---
+++ src/component/footer/footer.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../../assets/logo.png';
+
+import './footer.styles.scss';
+
+const Footer = () => (
+    <footer>
+        <section className='first-footer'>
+            <div className='container'>
+                <ul className='row'>
+                    <li className='col-md-4 row'>
+                        <img src="" alt=""/>
+                        <p>Safe shopping<br/>Buy with confidence</p>
+                    </li>
+                    <li className='col-md-4 row'>
+                        <img src="" alt=""/>
+                        <p>Fast shipping<br/>Get your product fast</p>
+                    </li>
+                    <li className='col-md-4 row'>
+                        <img src="" alt=""/>
+                        <p>Satisfaction guarantee<br/>Or get your money back</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section>
+            <div className='container'>
+                <ul className='row footer-links'>
+                    <li className='col-md-2'>
+                        <Link className='navbar-brand'><img src={Logo} alt=""/></Link>
+                    </li>
+                    <li className='col-md-2'>
+                        <Link>Home</Link>
+                    </li>
+                    <li className='col-md-2'>
+                        <Link>Collection</Link>
+                    </li>
+                    <li className='col-md-2'>
+                        <Link>Blog</Link>
+                    </li>
+                    <li className='col-md-2'>
+                        <Link>Sign In</Link>
+                    </li>
+                    <li className='col-md-2'>
+                        <Link>Cart</Link>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </footer>
+);
+
+export default Footer; 
